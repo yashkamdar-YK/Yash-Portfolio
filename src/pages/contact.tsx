99e7e6945611ec3b92fc854d1e74c2292f76000e
Yash Kamdar <kamdaryash7999@gmail.com>
2024-11-03 14:21:14 +0530

    Add Files

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,8 +6,8 @@ const contact = () => {
 		<div className="h-screen w-screen flex justify-center items-center text-base lg:text-lg gap-y-2 font-semibold dark:text-white">
 			<Layout.Default>
 				<div className="px-4 py-2 rounded-md border border-stone-600 w-fit  dark:bg-slate-900 bg-slate-300 bg-opacity-40 backdrop-blur-lg z-10">
-					<p className=" ">Email : ankit0panchal@gmail.com</p>
-					<p className="lg:self-center self-start">Phone : +91 99936 73521</p>
+					<p className=" ">Email : work.yashkamdar@gmail.com</p>
+					<p className="lg:self-center self-start">Phone : +91 7999080833</p>
 				</div>
 			</Layout.Default>
 		</div>
